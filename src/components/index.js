fa362ca5e59de343fa9cdf57adcf358c559134c2
iamaustin316 <iamaustin316@gmail.comm>
2018-05-07 19:11:41 +0800

    cool

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+export default class Section extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "你沒有按",
+    };
+  }
+  render() {
+    return (
+      <div className="box">
+          <h2>使用者：{this.props.name}</h2>
+          <p>手機：{this.props.mobile}</p>
+          <button className="square" onClick={() => this.setState({value: '你已經按了'})}>{this.state.value}</button>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import 'index.scss';
 
 const render = () => {
   const Main = require('containers').default;
-
+  const Section = require('components').default;
+  const nameAry = [
+    {
+      name: "Austin",
+      mobile: "0978-086-965"
+    },
+    {
+      name: "Jason",
+      mobile: "0928-216-235"
+    }
+    ,
+    {
+      name: "Andy",
+      mobile: "0911-465-757"
+    }
+  ];
   ReactDOM.render(
     <AppContainer>
-      <Main />
+      <div className={'wrap'}>
+        <Main />
+        {nameAry.map((item,index)=>{
+            return <Section key={index} name={item.name} mobile={item.mobile} />
+        })}
+        
+      </div>
     </AppContainer>,
     document.getElementById('app')
   );
